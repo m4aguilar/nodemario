112aa795349cf8dfb8ddf39cd9631a49bbca7ba9
m4aguilar <mario4.1990@gmail.com>
2017-11-29 18:51:20 +0100

    1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 app.set('port', (process.env.PORT || 8080))
 
 app.get('/', function(req, res){
-  res.json({ mensaje: 'Un ejemplo de nodejs, express y Heroku'});
+  res.json({ mensaje: 'Madre mía que guapa!!! <3 <3 <3 Rubí'});
 });
 
 app.listen(app.get('port'));
