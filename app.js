afab2a15dc6438fe52b211da47571135c4570d19
m4aguilar <mario4.1990@gmail.com>
2017-11-29 17:58:35 +0100

    1

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-const http = require('http');
+// index.js
+var express = require('express');
+var app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.set('port', (process.env.PORT || 8080))
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.get('/', function(req, res){
+  res.json({ mensaje: 'Un ejemplo de nodejs, express y Heroku'});
 });
 
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', http.address().port);
-});
+app.listen(app.get('port'));
